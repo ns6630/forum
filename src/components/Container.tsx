739996fe952e513d878f25509c0e859471c16903
Sergey Zabelin <ns6630@gmail.com>
2021-10-05 01:53:07 +0300

    Добавлен компонент регулирующий ширину контента страницы.

---
+++ src/components/Container.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components/macro";
+
+const Container = styled.div`
+  width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;
+
+  @media(max-width: 1200px) {
+    width: 100%;
+  }
+`
+
+export default Container;
