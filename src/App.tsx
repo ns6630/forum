9c42b3a110642dc0c7b2aec18c54692f20197e58
Sergey Zabelin <ns6630@gmail.com>
2021-11-01 02:00:24 +0300

    Изменен рендер домашней страницы.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import Loader from "./components/Loader";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faHouseChimney, faCompass, faCircleQuestion, faComment} from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleQuestion,
+  faComment,
+  faCompass,
+  faHouseChimney,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 import Container from "./components/Container";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Menu from "./components/Menu";
 import Home from "./pages/Home";
 import RightColumn from "./components/RightColumn";
@@ -14,47 +19,126 @@ import MenuLink from "./components/MenuLink";
 import Topics from "./pages/Topics";
 import MyTopics from "./pages/MyTopics";
 import MyAnswers from "./pages/MyAnswers";
+import RightColumnButtonLink from "./components/RightColumnButtonLink";
+import TopUsers from "./components/TopUsers";
+import Footer from "./components/Footer";
+import FooterLink from "./components/FooterLink";
+import StickyRightColumn from "./components/StickyRightColumn";
+import TopicApi from "./api/fake/TopicApi";
+import Page from "./components/Page";
 
+export const topicApi = new TopicApi();
 
 function App() {
   return (
     <BrowserRouter>
-      <Header>
+      <Page>
+        <Header user={you} />
         <Container>
-          <h1>Hello, is it me you looking for...</h1>
+          <Body>
+            <Menu>
+              <MenuLink to={"/"} exact>
+                <FontAwesomeIcon icon={faHouseChimney} fixedWidth />
+                Home
+              </MenuLink>
+              <MenuLink to={"/topics"}>
+                <FontAwesomeIcon icon={faCompass} fixedWidth />
+                Explore topics
+              </MenuLink>
+              <MenuLink to={"/my-topics"}>
+                <FontAwesomeIcon icon={faCircleQuestion} fixedWidth />
+                My topics
+              </MenuLink>
+              <MenuLink to={"/my-answers"}>
+                <FontAwesomeIcon icon={faComment} fixedWidth />
+                My answers
+              </MenuLink>
+            </Menu>
+            <Content>
+              <Switch>
+                <Route path={"/topics"}>
+                  <Topics />
+                </Route>
+                <Route path={"/my-topics"}>
+                  <MyTopics />
+                </Route>
+                <Route path={"/my-answers"}>
+                  <MyAnswers />
+                </Route>
+                <Route path={"/"}>
+                  <Home />
+                </Route>
+              </Switch>
+            </Content>
+            <RightColumn>
+              <StickyRightColumn>
+                <RightColumnButtonLink to={"/"}>
+                  <FontAwesomeIcon icon={faPlus} />
+                  Start a New Topic
+                </RightColumnButtonLink>
+                <TopUsers users={topUsers} you={you} />
+                <Footer>
+                  <FooterLink to={"/"}>Help</FooterLink>
+                  <FooterLink to={"/"}>About</FooterLink>
+                  <FooterLink to={"/"}>Forum Pro</FooterLink>
+                  <FooterLink to={"/"}>Careers</FooterLink>
+                  <FooterLink to={"/"}>Topics</FooterLink>
+                  <FooterLink to={"/"}>Press</FooterLink>
+                  <FooterLink to={"/"}>Top Topics</FooterLink>
+                  <FooterLink to={"/"}>Terms</FooterLink>
+                  <FooterLink to={"/"}>Blog</FooterLink>
+                  <FooterLink to={"/"}>Privacy policy</FooterLink>
+                  <FooterLink to={"/"}>Advertise</FooterLink>
+                </Footer>
+              </StickyRightColumn>
+              {/*<Loader />*/}
+            </RightColumn>
+          </Body>
         </Container>
-      </Header>
-      <Container>
-        <Body>
-          <Menu>
-            <MenuLink to={"/"} exact><FontAwesomeIcon icon={faHouseChimney}/>Home</MenuLink>
-            <MenuLink to={"/topics"}><FontAwesomeIcon icon={faCompass}/>Explore topics</MenuLink>
-            <MenuLink to={"/my-topics"}><FontAwesomeIcon icon={faCircleQuestion}/>My topics</MenuLink>
-            <MenuLink to={"/my-answers"}><FontAwesomeIcon icon={faComment}/>My answers</MenuLink>
-          </Menu>
-          <Content>
-            <Switch>
-              <Route path={"/topics"}>
-                <Topics/>
-              </Route>
-              <Route path={"/my-topics"}>
-                <MyTopics/>
-              </Route>
-              <Route path={"/my-answers"}>
-                <MyAnswers/>
-              </Route>
-              <Route path={"/"}>
-                <Home/>
-              </Route>
-            </Switch>
-          </Content>
-          <RightColumn>
-            <Loader/>
-          </RightColumn>
-        </Body>
-      </Container>
+      </Page>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+const topUsers = [
+  {
+    id: 1,
+    nickname: "Johny",
+    photoPath: "",
+    rating: 12142,
+  },
+  {
+    id: 2,
+    nickname: "Terry",
+    photoPath: "logo192.png",
+    rating: 2347,
+  },
+  {
+    id: 3,
+    nickname: "Body",
+    photoPath: "",
+    rating: 413,
+  },
+  {
+    id: 4,
+    nickname: "Tom",
+    photoPath:
+      "http://kartina-optom.com.ua/images/stories/virtuemart/product/50%D1%8550.jpg",
+    rating: 337,
+  },
+  {
+    id: 5,
+    nickname: "Jerry",
+    photoPath: "https://avatarko.ru/img/kartinka/33/igra_Minecraft_32501.jpg",
+    rating: 73,
+  },
+];
+
+const you = {
+  id: 1,
+  nickname: "Johny",
+  photoPath: "",
+  rating: 12142,
+};
