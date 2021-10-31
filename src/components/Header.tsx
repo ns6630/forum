aed14ffe50220b05a017d51e0c0cce89c887e9cc
Sergey Zabelin <ns6630@gmail.com>
2021-11-01 01:39:22 +0300

    Обновлен Header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,37 @@
 import styled from "styled-components/macro";
+import React from "react";
+import { User } from "../types/User";
+import Container from "./Container";
+import Logo from "./Logo";
 
-const Header = styled.div`
+export interface HeaderProps {
+  user: User;
+}
+
+const Header: React.FC<HeaderProps> = () => {
+  return (
+    <StickyHeader>
+      <Container>
+        <StyledHeader>
+          <Logo />
+        </StyledHeader>
+      </Container>
+    </StickyHeader>
+  );
+};
+
+const StickyHeader = styled.div`
   position: sticky;
   top: 0;
-  box-shadow: 0 0 5px rgba(0, 0, 0, .2);
+  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.1);
   overflow: auto;
-  background: #fcfcfc;
+  background: #ffffff;
+`;
+
+const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  height: 80px;
 `;
 
 export default Header;
