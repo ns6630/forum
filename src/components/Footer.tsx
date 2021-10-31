f87dd1cdb14d1f06a96bec050ddcc23532098b56
Sergey Zabelin <ns6630@gmail.com>
2021-11-01 01:46:17 +0300

    Добавлен компонент Footer и FooterLink.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components/macro";
+
+const Footer = styled.footer`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1em;
+  font-size: 0.7rem;
+  background: #ffffff;
+  border-radius: 5px;
+  box-shadow: 0 15px 15px -3px rgba(143, 143, 143, 0.1);
+  padding: 20px;
+`;
+
+export default Footer;
---
+++ src/components/FooterLink.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components/macro";
+import Link from "./Link";
+
+const FooterLink = styled(Link)`
+  color: #8f8f8f;
+  text-transform: capitalize;
+  font-weight: normal;
+`;
+
+export default FooterLink;
