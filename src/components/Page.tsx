0331dc098953366870b9e22e74bdc9e371daeec1
Sergey Zabelin <ns6630@gmail.com>
2021-11-01 01:45:41 +0300

    Добавлен компонент Page.

---
+++ src/components/Page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+const Page = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+export default Page;
